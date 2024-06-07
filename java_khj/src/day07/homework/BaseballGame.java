449bbb602b552a2c0335745ee1bfdee053a1199c
KimHyeonjung <kardel@naver.com>
2024-06-07 16:03:44 +0900

    2024 6월 7일 과제 - 야구게임 마무리

--- java_khj/src/day07/homework/BaseballGame.java
+++ java_khj/src/day07/homework/BaseballGame.java
@@ -27,13 +27,13 @@ public class BaseballGame {
 		 * */
 
 		//중복되지 않은 랜덤한 수(1~9) 3개를 저장하는 배열과 입력 받은 수 3개를 저장하는 배열 선언, 생성
-		int [] baseBallNum, iNum;		
+		int [] baseBallNum, userNum;		
 		int arrLength = 3; //게임에 사용할 숫자 개수 (배열의 길이)
 		int storedCount = 0, i;
 		int random, num;
 		int max = 9, min = 1;
 		baseBallNum = new int[arrLength];
-		iNum = new int[arrLength];
+		userNum = new int[arrLength];
 		Scanner scan = new Scanner(System.in);
 
 		//중복되지 않는 랜덤한 수 3개 생성해 baseBallNum에 저장
@@ -52,47 +52,64 @@ public class BaseballGame {
 		}
 		System.out.println(Arrays.toString(baseBallNum)); // 중복되지 않는 랜덤한 수 3개가 잘 들어갔는지 확인
 
-		//숫자 3개 입력받아 iNum에 저장
-		System.out.print("1~9까지의 숫자 3개를 중복되지 않게 입력(예 : 3 5 9) :  ");
-		for(i = 0; i < arrLength; i++) {
-			num = scan.nextInt();
-			iNum[i] = num;
-			storedCount++;
-		}
-		System.out.println(Arrays.toString(iNum)); // 중복되지 않는 랜덤한 수 3개가 잘 들어갔는지 확인
-		/* - 스트라이크 : 해당 숫자가 있고, 위치도 같은 경우
-		 * - 볼 : 숫자가 있지만 위치가 다른 경우
-		 * - 아웃 : 일치하는 숫자가 하나도 없는 경우
-		 */
+		
+		boolean isDupl = true;
 		int strikeCount = 0, ballCount = 0;
-		for(i = 0; i < arrLength ; i++) {
-			//스트라이크 체크
-			if(baseBallNum[i] == iNum[i]) {
-				strikeCount++;
-			}
-			//볼 체크
-			for(int j = 0; j < arrLength; j++) {
-				if(i == j) {
-					continue;
+		do {
+			//입력 안내 문구 출력
+			strikeCount = 0;
+			ballCount = 0;
+			System.out.print("입력 :  ");
+			while(isDupl) {	
+				//숫자 3개 입력받아 iNum에 저장
+				for(i = 0; i < arrLength; i++) {
+					num = scan.nextInt();
+					userNum[i] = num;
 				}
-				else if(baseBallNum[i] == iNum[j]) {
-					ballCount++;
+				for(i = 0; i < arrLength; i++) {
+					for(int j =0; j < i; j++) {
+						if (userNum[i] == userNum[j]) {  // 중복 검사
+		                    isDupl = true;
+						}
+						else {
+							isDupl = false;
+						}
+					}
+				}
+				if(isDupl) {
+					System.out.println("중복된 숫자가 있습니다. 다시 입력 : ");
 				}
 			}
-		}
-		if (strikeCount > 0 && ballCount > 0) {
-			System.out.print(strikeCount + "S" + ballCount + "B");
-		}
-		else if (strikeCount > 0 && ballCount == 0 ) {
-			System.out.print(strikeCount + "S");
-		}
-		else if (ballCount > 0 && strikeCount == 0) {
-			System.out.print(ballCount + "B");
-		}
-		else {
-			System.out.print("O");
-		}
-
+			//결과 판별 후 출력
+			for(i = 0; i < arrLength ; i++) {
+				//스트라이크와 볼 체크 
+				for(int j = 0; j < arrLength; j++) {
+					if(baseBallNum[i] == userNum[j]) {
+						if(i == j) {
+							strikeCount++;
+						}
+						else  {
+							ballCount++;
+						}
+					}
+				}
+			}
+			if (strikeCount == 3) {
+				System.out.print(strikeCount + "S");
+				System.out.println("  정답입니다.");
+			}			
+			else if (strikeCount > 0) {
+				System.out.println(strikeCount + "S");
+			}
+			else if (ballCount > 0) {
+				System.out.println(ballCount + "B");
+			}
+			else {
+				System.out.println("O");
+			}
+			isDupl = true;
+	
+		} while(strikeCount != 3) ;
 	}
 
 }
