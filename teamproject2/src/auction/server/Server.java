b885dd17c76a96b141fa6702270a9045a2e222f4
KimHyeonjung <kardel@naver.com>
2024-07-14 20:37:09 +0900

    팀 프로젝트 구현 중

--- teamproject2/src/auction/server/Server.java
+++ teamproject2/src/auction/server/Server.java
@@ -2,14 +2,20 @@ package auction.server;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 import auction.Item;
 import lombok.AllArgsConstructor;
@@ -20,12 +26,14 @@ import lombok.Data;
 public class Server {
 
 	private List<ObjectOutputStream> list = Collections.synchronizedList(new ArrayList<ObjectOutputStream>());
-	private List<Item> itemList = Collections.synchronizedList(new ArrayList<Item>());
+//	private List<Item> itemList = Collections.synchronizedList(new ArrayList<Item>());
 	private Socket socket;
 	private ObjectOutputStream oos;
 	private ObjectInputStream ois;
 	public final static String EXIT = "-quit";
-
+	private Item highestBid;
+	private File file = new File("src/auction/server/itemData.txt");
+	public static Scanner scan = new Scanner(System.in);
 
 	public Server(List<ObjectOutputStream> list, Socket socket) {
 		this.list = list;
@@ -43,26 +51,27 @@ public class Server {
 			//			} catch (Exception e) {
 			//				System.out.println("파일 저장 실패");
 			//			}
-
-			File file = new File("src/auction/server/itemData.txt");
 			loadItemList(file);
 
 		} catch (Exception e) {
 		}
 	}
-
+	
+	
 	public void timer(Instant finish) {
 		Thread thread = new Thread(()->{
 			while(finish.isAfter(Instant.now())) {
-				
+
 			}
 			System.out.println("[ 경매 종료 ]");
 			Item close = null;
 			sendAll(close);
+			Auctioneer.itemList.add(highestBid);
+			saveItemList(file);
 		});
 		thread.start();
 	}
-	
+
 	public void receive(Item item, Instant finish) {
 		Thread thread = new Thread(()->{
 			String id = "";
@@ -72,8 +81,8 @@ public class Server {
 				oos.writeObject(item);
 				oos.writeObject(finish); 
 				oos.flush();
-//				System.out.println("경매정보 전송");
-//				System.out.println(finish); //test
+				//				System.out.println("경매정보 전송");
+				//				System.out.println(finish); //test
 				while(true) { // true 대신 경매 시간 비교식 넣으면 될듯
 					id = ois.readUTF();
 					String str = ois.readUTF();
@@ -82,6 +91,7 @@ public class Server {
 						System.out.println("[" + id + "님 " + str + "원 입찰]");
 						//최고입찰가와 입찰자를 아이템 등록
 						Item updateBid = new Item(item.getName(), price, id);
+						highestBid = updateBid;
 						//메세지를 보낸 소켓을 제외한 다른 소켓에 메세지를 전송
 						sendAll(updateBid);
 					} else {
@@ -101,23 +111,42 @@ public class Server {
 	public void sendAll(Item updateBid) {
 		for(ObjectOutputStream tmp : list) {
 			//메세지를 보낸 소켓을 제외한 다른 소켓에 메세지를 전송
-//			if(tmp != oos) { //본인도 확인 가능하도록 함
-				try {
-					tmp.writeObject(updateBid);
-					tmp.flush();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-//			}
+			//			if(tmp != oos) { //본인도 확인 가능하도록 함
+			try {
+				tmp.writeObject(updateBid);
+				tmp.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//			}
 		}
 	}
 
 	@SuppressWarnings("unchecked")
 	private void loadItemList(File file) {
 		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
-			itemList = (List<Item>)ois.readObject();
+			Auctioneer.itemList = (List<Item>)ois.readObject();
 		} catch (Exception e) {
 			System.out.println("파일 불러오기 실패");
+		}
+	}
+
+	private void saveItemList(File file) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+			oos.writeObject(Auctioneer.itemList);
+			oos.close();
+		} catch (Exception e) {
+			System.out.println("파일 저장 실패");
+		}
+	}
+	//정수 말고 다른거 입력했을 때 예외 처리
+	public int nextInt() {
+		try {
+			return scan.nextInt();
+		} catch (InputMismatchException e) {
+			scan.nextLine();
+			return Integer.MIN_VALUE;
 		}
 	}
 
