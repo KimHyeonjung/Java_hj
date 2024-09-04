67f884b0b4ee6cfa4bfe1eb52341cce0951d0348
KimHyeonjung <kardel@naver.com>
2024-09-04 11:38:38 +0900

    스프링 08일차 : 프로젝트 생성 및 log4j 적용

---
+++ spring project/spring3/src/main/java/kr/kh/spring3/HomeController.java
@@ -0,0 +1,22 @@
+package kr.kh.spring3;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		log.info("/ : 메인 페이지입니다.");
+		return "home";
+	}
+	
+}
