52adc007210b1c1c8b734178f24833bf7dfdd6d1
KimHyeonjung <kardel@naver.com>
2024-09-04 12:30:23 +0900

    스프링 08일차 : 복습 - DB연동

---
+++ spring project/spring3/src/main/java/kr/kh/spring3/dao/MemberDAO.java
@@ -0,0 +1,8 @@
+package kr.kh.spring3.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface MemberDAO {
+
+	
+}
---
+++ spring project/spring3/src/main/java/kr/kh/spring3/model/vo/MemberVO.java
@@ -0,0 +1,10 @@
+package kr.kh.spring3.model.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberVO {
+	private String me_email;
+}
