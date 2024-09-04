6157bde24ba2c7830a097ea4646518dc50b17fb5
KimHyeonjung <kardel@naver.com>
2024-09-04 09:41:26 +0900

    스프링 08일차 : 로그인 후 이전 URL로 이동

--- spring project/spring_s/src/main/java/kr/kh/spring/interceptor/GuestInterceptor.java
+++ spring project/spring_s/src/main/java/kr/kh/spring/interceptor/GuestInterceptor.java
@@ -1,35 +1,43 @@
 package kr.kh.spring.interceptor;
 
+import java.util.Date;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import kr.kh.spring.model.vo.MemberVO;
 
-//로그인 상태에서 로그인,회원가입 페이지에 접근할 수 없게 하기 위해서
+//로그인후 이전 페이지로 이동할 때
 public class GuestInterceptor extends HandlerInterceptorAdapter{
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, 
-			HttpServletResponse response, 
-			Object handler)
-			throws Exception {
-			//로그인한 회원 정보를 가져옴
-			MemberVO user = (MemberVO)request.getSession().getAttribute("user");
-			//로그인 했으면 메인페이지로
-			if(user != null) {
-				response.setContentType("text/html; charset=UTF-8");
-				response.getWriter().write("<script>alert(\"회원은 접근할 수 있습니다.\")</script>");
-				response.getWriter().write("<script>location.href=\""+ request.getContextPath() + "/" + "\"</script>");
-				response.flushBuffer();
-				return false;
-			}
-			//아니면 가려던 길로 
-			else {
-				return true;
-			}
-			
+	public void postHandle(
+	    HttpServletRequest request, 
+	    HttpServletResponse response, 
+	    Object handler, 
+	    ModelAndView modelAndView)
+	    throws Exception {
+		HttpSession session = request.getSession();
+		//세션에 이전 URL 정보를 가져옴
+		String prevUrl = (String)session.getAttribute("prevUrl");
+		//이전 URL이 없으면 종료
+		if(prevUrl == null) {
+			return;
+		}
+		//로그인한 회원정보를 가져옴
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		//로그인한 회원 정보가 없으면 종료
+		if(user == null) {
+			return;
+		}
+		//이전 URL로 리다이렉트 하고 이전 URL을 제거
+		response.sendRedirect(prevUrl);
+		session.removeAttribute("prevUrl");
 	}
 	
 }
