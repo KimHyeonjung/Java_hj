774f720e74a23b1c4e3568bcadc0018360d544c8
KimHyeonjung <kardel@naver.com>
2024-09-19 10:27:24 +0900

    리액트 05일차 :  useEffect 예제

--- react/react1/src/index.js
+++ react/react1/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import RouteApp from './route/RouteApp';
+import UseEffectEx from './useEffectEx/UseEffectEx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     // <App />
-    <RouteApp />
+    // <RouteApp />
+    <UseEffectEx />
   
 );
 
