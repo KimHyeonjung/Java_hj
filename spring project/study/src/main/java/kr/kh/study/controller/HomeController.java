937a39f86ef74697c24e741d16db157eba50a0fc
KimHyeonjung <kardel@naver.com>
2024-09-20 15:18:11 +0900

    스프링 : xml 파일 없이 설정 연습 - gmail을 이용한 이메일 전송(config.properties이용 )

--- spring project/study/src/main/java/kr/kh/study/controller/HomeController.java
+++ spring project/study/src/main/java/kr/kh/study/controller/HomeController.java
@@ -2,7 +2,11 @@ package kr.kh.study.controller;
 
 import java.util.Locale;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,12 +20,14 @@ import kr.kh.study.dao.PostDAO;
 @Controller
 public class HomeController {
 	
-	
 	@Autowired
 	PostDAO postDao;
 	
 	@Autowired
 	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	@GetMapping("/")
 	public String home(Locale locale, Model model) {
@@ -32,6 +38,8 @@ public class HomeController {
 		System.out.println("암호화 된 문자열 : " + enc);
 		System.out.println("암호화된 문자열 == abc : " + passwordEncoder.matches("abc", enc));
 		System.out.println("암호화된 문자열 == abd : " + passwordEncoder.matches("abd", enc));
+		//메일 전송 테스트
+		mailSend("kardel@naver.com", "콘피그프로퍼티스 테스트", "전송이 잘 됐습니다");
 		return "/home";
 	}
 	
@@ -46,5 +54,26 @@ public class HomeController {
 	@GetMapping("/post/insert")
 	public String postInsert() {
 		return "home";
+	}
+	
+	public boolean mailSend(String to, String title, String content) {
+
+	    String setfrom = "kardel@naver.com";
+	   try{
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper
+	            = new MimeMessageHelper(message, true, "UTF-8");
+
+	        messageHelper.setFrom(setfrom);// 보내는사람 생략하거나 하면 정상작동을 안함
+	        messageHelper.setTo(to);// 받는사람 이메일
+	        messageHelper.setSubject(title);// 메일제목은 생략이 가능하다
+	        messageHelper.setText(content, true);// 메일 내용
+
+	        mailSender.send(message);
+	        return true;
+	    } catch(Exception e){
+	        e.printStackTrace();
+	        return false;
+	    }
 	}
 }
