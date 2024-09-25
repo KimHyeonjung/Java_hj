79839198f7f7cb6559f644fe98e96591e2c2358d
KimHyeonjung <kardel@naver.com>
2024-09-25 12:38:25 +0900

    부트 02일차 : 게시글 목록 조회 페이지네이션 적용

--- spring boot project/boot/src/main/java/kr/kh/boot/controller/PostController.java
+++ spring boot project/boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import kr.kh.boot.model.vo.CommunityVO;
 import kr.kh.boot.model.vo.PostVO;
+import kr.kh.boot.pagination.PageMaker;
+import kr.kh.boot.pagination.PostCriteria;
 import kr.kh.boot.service.PostService;
 import lombok.AllArgsConstructor;
 
@@ -20,13 +22,18 @@ public class PostController {
 	private PostService postService;
 	
 	@GetMapping("/post/list/{co_num}") // @PathVariable("co_num") ()생략 가능
-	public String postList(Model model, @PathVariable int co_num) {
+	public String postList(Model model, @PathVariable int co_num, PostCriteria cri) {
+		cri.setCo_num(co_num);
+		cri.setPerPageNum(2);
 		//커뮤니티 번호에 맞는 게시글 목록을 가져와서 화면에 전달
-		List<PostVO> list = postService.getPostList(co_num);
+		List<PostVO> list = postService.getPostList(cri);
 		//커뮤니티 목록을 가져와서 화면에 전달
 		List<CommunityVO> communityList = postService.getCommunityList();
+		//현재 페이지 정보를 이용해서 페이지메이커를 가져와서 화면에 전달
+		PageMaker pm = postService.getPageMaker(cri);
 		model.addAttribute("list", list);
 		model.addAttribute("cList", communityList);
+		model.addAttribute("pm", pm);
 		return "post/list";
 	}
 }
